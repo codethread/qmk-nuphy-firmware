f806c8ad8ba7ffb9087fe2bd3187387a4cf84e7d
codethread <adamhalldesigns@gmail.com>
2024-09-14 19:50:00 +0100

    wip

--- keyboards/nuphy/halo75v2/ansi/config.h
+++ keyboards/nuphy/halo75v2/ansi/config.h
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #define TAPPING_TERM_PER_KEY
-#define QUICK_TAP_TERM 150
+#define QUICK_TAP_TERM_PER_KEY
 #define PERMISSIVE_HOLD
 
 // Theirs
